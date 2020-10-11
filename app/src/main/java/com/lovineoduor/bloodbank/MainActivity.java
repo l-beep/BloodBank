a0e5dafc00a32917b9c69ccaaaf8021644f7a814
LOvine <oduor.lovine@gmail.com>
2020-10-11 17:22:33 +0300

    add a v7 support

--- app/src/main/java/com/lovineoduor/bloodbank/MainActivity.java
+++ app/src/main/java/com/lovineoduor/bloodbank/MainActivity.java
@@ -1,8 +1,9 @@
 package com.lovineoduor.bloodbank;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void request_blood(View view)
+    {
+        Intent intent=new Intent(this,RequestActivity.class);
+        startActivity(intent);
+    }
+
+    public void register_doner(View view)
+    {
+        Intent intent=new Intent(this,DonateActivity.class);
+        startActivity(intent);
+    }
+
+
 }
