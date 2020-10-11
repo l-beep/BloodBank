f68fc0653d60b2947e499bb36f00f0972cb98c58
LOvine <oduor.lovine@gmail.com>
2020-10-11 17:43:19 +0300

    add bundles

--- app/src/main/java/com/lovineoduor/bloodbank/MainActivity.java
+++ app/src/main/java/com/lovineoduor/bloodbank/MainActivity.java
@@ -1,29 +1,17 @@
-package com.lovineoduor.bloodbank;
-
-import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
-    public void request_blood(View view)
-    {
-        Intent intent=new Intent(this,RequestActivity.class);
-        startActivity(intent);
-    }
-
-    public void register_doner(View view)
-    {
-        Intent intent=new Intent(this,DonateActivity.class);
-        startActivity(intent);
-    }
-
-
 }
